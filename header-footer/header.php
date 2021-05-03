d060e5839420e0f600c4d40c33fdb70b32c04748
kevinkranenburg <25953@ma-web.nl>
2021-05-03 13:52:05 +0200

    Header menu fix

--- header-footer/header.php
+++ header-footer/header.php
@@ -3,32 +3,37 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<title>ido shop</title>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Author" content="Kevin Kranenburg">
-    <meta name="description" content="Here is a precise description of my awesome webpage.">
-    <meta name="robots" content="index, follow" />
-    <link rel="stylesheet" href="header-footer\css\header.css">
+    <link rel="stylesheet" href="../header-footer/css/header.css">
     <script src="https://kit.fontawesome.com/8ee2ef1015.js" crossorigin="anonymous"></script>
 </head>
 <body>
     
     <div class="header">
-        <img class="header-logo" src="../header-footer/assets/logo-sideways.png" alt="ido-logo">
-
-            <a href="/books">Shop</a>
-            <a href="/info">How does it work</a>
-            <a href="/about">About us</a>
-            <a href="/contact">Contact</a>
-
-        <div class="header-right">
-            <i class="icon-header fas fa-shopping-cart"></i>
-            <i class="icon-header fas fa-user"></i>
+        <div class="grid-container">
+            <div class="header-left">
+                <img class="header-logo" src="../header-footer/assets/logo-sideways.png" alt="ido-logo">
+            </div>
+            <div class="header-center">
+                <div class="header-menu">
+                    <a href="/books">Shop</a>
+                    <a href="/info">How does it work</a>
+                    <a href="/about">About us</a>
+                    <a href="/contact">Contact</a>
+                </div>
+            </div>
+            <div class="header-right">
+                <i class="icon-header fas fa-shopping-cart"></i>
+                <i class="icon-header fas fa-user"></i>
+            </div>
         </div>
     </div>
+
+    
 
 </body>
 </html>
