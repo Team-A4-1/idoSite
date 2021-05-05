94e2ab1cf4656341131989448a07b2c79b481aac
kevinkranenburg <25953@ma-web.nl>
2021-05-05 11:13:32 +0200

    footer responsive for all devices

--- header-footer/footer.php
+++ header-footer/footer.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     
-    <div class="footer">
+    <!-- <div class="footer">
         
     <div class="grid-container">
         <div class="footer-left">
@@ -47,7 +47,60 @@
         <div class="footer-copyright">
             <p>Copyright @ 2021 Librerio Tia Libro</p>
         </div>
+    </div> -->
+
+    <div class="container"></div>
+<footer>
+  <!-- Footer main -->
+  <section class="ft-main">
+    <div class="ft-main-item">
+      <div class="logo-footer-circle">
+                <img class="logo-footer" src="../header-footer/assets/logo-middle.png" alt="logo">
+            </div>
+            <div class="footer-left-text">
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor.
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor.</p>
+            </div>
     </div>
+
+    <div class="ft-main-item">
+      <h2 class="ft-title">MENU</h2>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Shop</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">How Does It Work</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+    <div class="ft-main-item">
+      <h2 class="ft-title">NEWSLETTER</h2>
+      <p>Subscribe to our newsletter and never miss new books</p>
+      <form>
+        <input type="email" name="email" placeholder="Your email address">
+        <input type="submit" value="SEND">
+      </form>
+    </div>
+  </section>
+
+  <!-- Footer social -->
+  <section class="ft-social">
+    <ul class="ft-social-list">
+      <li><a href="#"><i class="fab fa-2x fa-facebook"></i></a></li>
+    </ul>
+  </section>
+
+  <!-- Footer legal -->
+  <section class="ft-legal">
+    <ul class="ft-legal-list">
+      <li><a href="#">Terms &amp; Conditions</a></li>
+      <li><a href="#">Privacy Policy</a></li>
+      <li>&copy; 2021 Copyright Librerio Tia Libro</li>
+    </ul>
+  </section>
+</footer>
 
 </body>
 </html>
