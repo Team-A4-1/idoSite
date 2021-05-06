d5e7a1d8181d7fa2e1daf2831b83a2b0e27407a6
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2021-05-06 13:24:10 +0200

    added sql to be used in backend for checking password and username whit login

---
+++ views/test.sql
@@ -0,0 +1,10 @@
+-- password sql
+SELECT users.id 
+from users 
+Inner JOIN usernames on users.username=usernames.id 
+Inner JOIN passwords on users.username=passwords.id
+WHERE usernames.username = "John" AND passwords.password ="Pass123"
+
+--add new user
+INSERT INTO `usernames` (username) VALUES ('Samson')
+INSERT INTO `passwords` (password) VALUES ('Gertje69')
