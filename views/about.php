39f68108db4667340f6098c82ffc42db4c4d0af9
kevinkranenburg <25953@ma-web.nl>
2021-05-10 15:16:17 +0200

    About pagina + responsive

--- views/about.php
+++ views/about.php
@@ -1,7 +1,51 @@
-<?php
-require_once('header-footer/header.php');
-?>
-about
-<?php
-require_once('header-footer/footer.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Kevin Kranenburg">
+    <link rel="stylesheet" href="../css/about.css">
+    <title>About Us</title>
+</head>
+
+<?php require_once('header-footer/header.php'); ?>
+
+<body>
+    
+
+    <div class="wrap">
+        <div class="grid-container">
+            <div class="middle-box">
+                <div class="box-header">Our Story</div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. 
+                Cum sociis natoque penatibus et magnis dis parturient montes, 
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+                pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+            <div class="left-box">
+                <div class="box-header">About Us</div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. 
+                Cum sociis natoque penatibus et magnis dis parturient montes, 
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+                pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+            <div class="right-box">
+                <div class="box-header">History</div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. 
+                Cum sociis natoque penatibus et magnis dis parturient montes, 
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+                pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+        </div>
+    </div>
+
+    
+</body>
+
+<?php require_once('header-footer/footer.php'); ?>
+
+</html>
