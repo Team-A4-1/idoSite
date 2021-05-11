761802a227a92f6d0ef26c0ae1c9646dd06421ba
= <=>
2021-05-11 10:16:23 +0200

    Home - Blen
    Ik heb een begin gemaakt aan de home. en heb wat images geupload.

--- views/home.php
+++ views/home.php
@@ -1,7 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Blen Michil">
+    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="../css/about.css">
+    <title>Home</title>
+</head>
 <?php
 require_once('header-footer/header.php');
 ?>
-home
+
+<body>
+<div class="wrapper">
+    <section class="banner">
+        <div class="banner__wrapper">
+            <div class="banner__content">
+                <ul class="banner__list">
+                    <li class="banner__item">
+                        <div class="banner__title"><h1>Title</h1></div>
+                        <div class="banner__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, voluptatibus.</div>
+                        <div class="banner__btn"><a href="#">Buy now!</a></div>
+                    </li>
+                    <li class="banner__item">
+                        right
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="banner">best-sellers</section>
+    <section class="banner">advertisment</section>
+    <section class="banner">random-books</section>
+
+    <!-- <div class="wrap">
+        <div class="grid-container">
+            <div class="middle-box">
+                <div class="box-header">Our Story</div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. 
+                Cum sociis natoque penatibus et magnis dis parturient montes, 
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+                pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+            <div class="left-box">
+                <div class="box-header">About Us</div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. 
+                Cum sociis natoque penatibus et magnis dis parturient montes, 
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+                pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+            <div class="right-box">
+                <div class="box-header">History</div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aenean commodo ligula eget dolor. Aenean massa. 
+                Cum sociis natoque penatibus et magnis dis parturient montes, 
+                nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+                pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+        </div>
+    </div> -->
+
+</div>
+</body>
 <?php
 require_once('header-footer/footer.php');
 ?>
+
+</html>
