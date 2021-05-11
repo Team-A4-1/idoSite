6033f8d7deaee8d8869c06c6c37a3396df54a53d
kevinkranenburg <25953@ma-web.nl>
2021-05-11 20:13:54 +0200

    Contact page + responsive

--- views/contact.php
+++ views/contact.php
@@ -24,9 +24,15 @@
     Cum sociis natoque penatibus et magnis dis parturient montes, 
     nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
     pretium quis, sem. Nulla consequat massa quis enim.</p>
-    <i class="contact-icon fas fa-envelope"></i>
+    <div class="icon-box">
+        <a href="#" class="contact-icon fas fa-envelope"></a>
+        <p class="icon-text">example@mail.com</p>
+    </div>
     <br>
-    <i class="contact-icon fab fa-facebook-square"></i>
+    <div class="icon-box">
+        <a href="#" class="contact-icon fab fa-facebook-square"></a>
+        <p class="icon-text">Librerio Tia Libro</p>
+    </div>
 </div>
 
   </div>
@@ -36,14 +42,16 @@
         <p class="title-contact">SAY SOMETHING</p>
         <input class="contact-input" type="text" placeholder="Your Name..."><br>
         <input class="contact-input" type="text" placeholder="Your Email..."><br>
-        <p class="contact-text">Your Message</p>
+        <p class="contact-text-2">Your Message</p>
         <textarea class="contact-textarea" type="text" rows="8" cols="50" placeholder="..."></textarea><br>
-        <input class="contact-btn" type="button" value="SEND">
+        <div class="contact-btn-box">
+            <input class="contact-btn" type="button" value="SEND">
+        </div>
     </div>
   </div>
 </div>
     
-<script src="views/scripts/contact/mail.js"></script>
+
 <div class="contact-page-bottom">
     <div class="bottom-info-box">
     <div class="logo-footer-circle">
@@ -51,20 +59,26 @@
     </div>
     <div class="footer-left-text">
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-        Aenean commodo ligula eget dolor.
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-        Aenean commodo ligula eget dolor.</p>
+        Aenean commodo ligula eget dolor. Aenean massa. 
+        Cum sociis natoque penatibus et magnis dis parturient montes, 
+        nascetur ridiculus mus. Donec quam felis, 
+        ultricies nec, pellentesque eu, 
+        pretium quis, sem. Nulla consequat massa quis enim.</p>
     </div>
 
 </div>
 <div class="bottom-menu">
-        <ul>
-            <li><a href="/">HOME</a></li>
-            <li><a href="/books">SHOP</a></li>
-            <li><a href="/about">ABOUT US</a></li>
-            <li><a href="/info">HOW DOES IT WORK</a></li>
-            <li><a href="/contact">CONTACT</a></li>
-        </ul>
+    <ul>
+        <li><a href="/">HOME</a></li>
+        <li><a href="/books">SHOP</a></li>
+        <li><a href="/about">ABOUT US</a></li>
+        <li><a href="/info">HOW DOES IT WORK</a></li>
+        <li><a href="/contact">CONTACT</a></li>
+    </ul>
+
+        <div class="copy-bottom">
+            <p>&copy; 2021 Copyright Librerio Tia Libro</p>
+        </div>
     </div>
 </div>
 
