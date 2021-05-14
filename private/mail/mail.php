7e24afb6e04a4ffec555d25aa63126ea6edf74de
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2021-05-14 16:49:24 +0200

    mail function

--- private/mail/mail.php
+++ private/mail/mail.php
@@ -1,6 +1,24 @@
 <?php
-$mailadres = "";
+
+use function PHPSTORM_META\elementType;
+
+$mailadres = "florian.tjeertes@gmail.com";
 $clientAdres =$_POST["ca"];
 $clientName =$_POST["cn"];
-echo $clientAdres;
-echo $clientName;
+
+$message = '<html><body>';
+$message .='<header> </header>';
+$message .='<h1>hey dit is een email. VET! </h1>';
+$message .='</footer>';
+
+$headers = 'From: '.$clientAdres . "\r\n" .
+    'Reply-To: '. $clientAdres . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if( mail($mailadres, $clientAdres, $message, $headers)){
+    echo "mail send";
+
+}
+else{
+    echo "mail not send";
+}
--- public/scripts/contact/mail.js
+++ public/scripts/contact/mail.js
@@ -5,20 +5,19 @@ let submit = document.getElementsByClassName("contact-btn");
 
 
 
-function mailSend(){
+function mailSend(clientAdress,clientName){
 var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     //  error(this.responseText);
     }
-    else if(this.status>200){
-      // error("ajax error"+this.readyState+" "+this.status);
+    // else if(this.status>200){
+    //   error("ajax error"+this.readyState+" "+this.status);
 
-    }
+    // }
    
   };
   xhttp.open("POST", "private/mail/mail.php", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("ca=Henry&cn=Ford"); 
-  xhttp.send();
+  xhttp.send("ca="+clientAdress+"&cn="+clientName); 
 }
