2db7c8623b46ad6796d71de7fe9a0dbb102983a8
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2021-05-15 17:00:21 +0200

    mail function

--- private/mail/mail.php
+++ private/mail/mail.php
@@ -5,20 +5,42 @@ use function PHPSTORM_META\elementType;
 $mailadres = "florian.tjeertes@gmail.com";
 $clientAdres =$_POST["ca"];
 $clientName =$_POST["cn"];
+$clientMessage =$_POST["cn"];
 
-$message = '<html><body>';
-$message .='<header> </header>';
-$message .='<h1>hey dit is een email. VET! </h1>';
-$message .='</footer>';
 
-$headers = 'From: '.$clientAdres . "\r\n" .
-    'Reply-To: '. $clientAdres . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+
+$message = 
+'<html>
+   <body>
+    <header> 
+    </header>
+        <h1>hey dit is een email. VET! </h1>
+        <p>'.$clientMessage.'</p>
+    <footer>
+    </footer>
+    </body>
+</html>';
+
+$headers = array(
+    'From' => $clientAdres,
+    'Reply-To' => $clientAdres,
+    'X-Mailer' => 'PHP/' . phpversion(),
+    'MIME-Version' => '1.0',
+    'Content-type'=> 'text/html; charset=iso-8859-1',
+
+);
+
 
 if( mail($mailadres, $clientAdres, $message, $headers)){
-    echo "mail send";
-
+ReturnJason(true,'succes');
 }
 else{
-    echo "mail not send";
+    ReturnJason(true,'failure');
 }
+function ReturnJason(bool $test,string $message){
+$myObj->status = $test;
+$myObj->message =$message;
+header("Content-Type: application/json; charset=UTF-8");
+$myJSON = json_encode($myObj);
+echo $myJSON;
+};
--- public/scripts/contact/mail.js
+++ public/scripts/contact/mail.js
@@ -1,23 +1,35 @@
 let contactInput = document.getElementsByClassName("contact-input");
-let contactMessage = document.getElementsByClassName("contact-text-2");
-let submit = document.getElementsByClassName("contact-btn"); 
+let contactMessage = document.getElementsByClassName("contact-textarea");
+
+let submit = document.getElementsByClassName("contact-btn")[0]; 
+
+submit.onClick= function(){ mailSend(
+  contactInput[1],contactInput[0],contactMessage[0]
+   )
+};
 
 
 
 
-function mailSend(clientAdress,clientName){
+
+
+function mailSend(clientAdress,clientName,clientMessage){
+
+  console.log('test');
+
+
 var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     //  error(this.responseText);
     }
-    // else if(this.status>200){
-    //   error("ajax error"+this.readyState+" "+this.status);
+    else if(this.status>200){
+      // error("ajax error"+this.readyState+" "+this.status);
 
-    // }
+    }
    
   };
   xhttp.open("POST", "private/mail/mail.php", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("ca="+clientAdress+"&cn="+clientName); 
+  xhttp.send("ca="+clientAdress+"&cn="+clientName+"&cm="+clientMessage); 
 }
