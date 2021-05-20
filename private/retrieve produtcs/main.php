94762f4f0eba5d7a2246cee7e3e86d00a18eebe6
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2021-05-20 12:20:52 +0200

    file paths

---
+++ private/retrieve produtcs/main.php
@@ -0,0 +1,19 @@
+<?php
+namespace products{
+class main{
+    private $sort;
+    private $lastName;
+    private $emailAddress;
+
+    function __construct($name) {
+        $this->name = $name;
+      }
+      function __destruct() {
+
+
+    }
+
+}
+
+}
+?>
