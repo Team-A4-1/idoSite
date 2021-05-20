21a9c9fc738de38cc6abecbec9306e06997c46ee
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2021-05-20 15:29:07 +0200

    start get set script produtcs

---
+++ private/produtcs/getData.php
@@ -0,0 +1,23 @@
+<?php
+namespace products{
+class getData{
+
+
+    function  text(string $search){
+
+        $result = null;
+        
+      
+        return $result;
+    }
+    function  id(int $id ,array $sort = ['asc'=>true,'price'=>null]){
+        $result = null;
+
+       
+        return $result;
+    }
+ 
+
+}    
+}
+?>
---
+++ private/produtcs/main.php
@@ -0,0 +1,41 @@
+<?php
+namespace products;
+include "getData.php";
+include "setData.php";
+
+class main{
+    private string $search;
+    private int $get;
+
+    function __construct($search,$sort) {
+        $this->search = $_POST[''];
+        $this->get = $_POST[''];
+
+        $this -> main();
+
+      }
+    function main(){
+        if($this->get){
+
+
+        }
+      elseif($this->get){
+
+      }
+    }
+
+
+
+
+
+
+
+      function __destruct() {
+        $this-> search=null;
+        $this-> sort=null;
+    }
+
+}
+
+
+?>
