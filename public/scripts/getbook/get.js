f5d38d000bac812e398533f70b9d91be3f204225
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2021-05-30 13:50:46 +0200

    boeken pagina voorbeeld

--- public/scripts/getbook/get.js
+++ public/scripts/getbook/get.js
@@ -1,19 +1,73 @@
 function get(){
 
 
-
+var status;
     var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var json = JSON.parse(xhttp.responseText);
           console.log(json);
+          status = json['status'];
+          delete json['status'];
+          
+           for(var i =0;i<Object.keys(json).length;i++){
+            var  element = document.createElement("div");
+           
+            //title
+            var  title = document.createElement("div");
+            //main img
+            var  img = document.createElement("img");
+
+            //un orderd list atributes
+            var atributes = document.createElement('ul');
+            var height = document.createElement('li');
+            var width = document.createElement('li');
+            var depth = document.createElement('li');
+            var price = document.createElement('li');
+
+
+
+
+           
+
+          //append data to title
+            var titletext = document.createTextNode(json[i]['name']);
+          //append img to img
+             img.src=json[i]['imageLOWres'];
+          
+          height.innerText="height: "+json[i]['height'];
+          width.innerText="width: "+json[i]['width'];
+          depth.innerText="depth: "+json[i]['depth'];
+          price.innerText="price: "+json[i]['price'];
+
+          //append text
+             title.appendChild(titletext)
+
+          //append atributes
+          atributes.appendChild(height); 
+          atributes.appendChild(width); 
+          atributes.appendChild(depth); 
+          atributes.appendChild(price); 
+
+
+          //append to main element
+            element.appendChild(title); 
+            element.appendChild(img); 
+            element.appendChild(atributes); 
+
+
+
+              document.getElementById('test').appendChild(element);
+
+           };
+
         }
      
       };
 var data ={
 'post': true,
 'data': {
-  'name':'boek',
+  'name':'',
    'id':null
 },
 'atributes':{
--- public/views/books.php
+++ public/views/books.php
@@ -2,6 +2,10 @@
 require_once('header-footer/header.php');
 ?>
 books
+<!-- voorbeeld -->
+<div id="test">
+</div>
+<!-- einde voorbeeld -->
 <script src="public\scripts\getbook\get.js"></script>
 <?php
 require_once('header-footer/footer.php');
