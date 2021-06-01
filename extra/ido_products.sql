59f38ec47523cd2a6b3f1c19e4a02e228125e3e9
kevinkranenburg <25953@ma-web.nl>
2021-06-01 15:57:11 +0200

    Product details style setup

--- extra/ido_products.sql
+++ extra/ido_products.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Gegenereerd op: 25 mei 2021 om 09:44
--- Serverversie: 10.1.37-MariaDB
--- PHP-versie: 7.2.12
+-- Host: 127.0.0.1
+-- Gegenereerd op: 01 jun 2021 om 15:47
+-- Serverversie: 10.4.18-MariaDB
+-- PHP-versie: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +18,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ido_products`
+-- Database: `ido products`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `author` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `authors`
+--
+
+INSERT INTO `authors` (`id`, `author`) VALUES
+(1, 'john doe');
 
 -- --------------------------------------------------------
 
@@ -52,15 +69,20 @@ CREATE TABLE `products` (
   `name` text NOT NULL,
   `atributes` int(11) NOT NULL,
   `text` text NOT NULL,
-  `image` int(11) NOT NULL DEFAULT '0'
+  `image` int(11) NOT NULL DEFAULT 0,
+  `author` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `atributes`, `text`, `image`) VALUES
-(2, 'boek', 1, 'dit is een test boek', 1);
+INSERT INTO `products` (`id`, `name`, `atributes`, `text`, `image`, `author`) VALUES
+(2, 'boek', 1, 'dit is een test boek', 1, 1),
+(3, 'boek test 2', 2, 'dit is nog een test boek', 1, 1),
+(4, 'boek test 3', 2, 'dit is nog een test boek', 1, 1),
+(5, 'boek test 4', 2, 'dit is nog een test boek', 1, 1),
+(6, 'boek test 5', 2, 'dit is nog een test boek', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -70,22 +92,31 @@ INSERT INTO `products` (`id`, `name`, `atributes`, `text`, `image`) VALUES
 
 CREATE TABLE `product_atributes` (
   `id` int(11) NOT NULL,
-  `height` int(11) NOT NULL DEFAULT '0',
-  `width` int(11) NOT NULL DEFAULT '0',
-  `depth` int(11) NOT NULL DEFAULT '0',
-  `price` int(11) NOT NULL DEFAULT '0'
+  `height` int(11) NOT NULL DEFAULT 0,
+  `width` int(11) NOT NULL DEFAULT 0,
+  `depth` int(11) NOT NULL DEFAULT 0,
+  `price` int(11) NOT NULL DEFAULT 0,
+  `amount` int(11) NOT NULL,
+  `weight` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `product_atributes`
 --
 
-INSERT INTO `product_atributes` (`id`, `height`, `width`, `depth`, `price`) VALUES
-(1, 0, 0, 0, 0);
+INSERT INTO `product_atributes` (`id`, `height`, `width`, `depth`, `price`, `amount`, `weight`) VALUES
+(1, 0, 0, 0, 0, 1, 0),
+(2, 100, 100, 50, 20, 0, 0);
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
+
+--
+-- Indexen voor tabel `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `images`
@@ -99,7 +130,8 @@ ALTER TABLE `images`
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `images` (`image`),
-  ADD KEY `atributes` (`atributes`);
+  ADD KEY `atributes` (`atributes`),
+  ADD KEY `author` (`author`);
 
 --
 -- Indexen voor tabel `product_atributes`
@@ -112,22 +144,28 @@ ALTER TABLE `product_atributes`
 --
 
 --
+-- AUTO_INCREMENT voor een tabel `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT voor een tabel `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT voor een tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT voor een tabel `product_atributes`
 --
 ALTER TABLE `product_atributes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
@@ -138,6 +176,7 @@ ALTER TABLE `product_atributes`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `atributes` FOREIGN KEY (`atributes`) REFERENCES `product_atributes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `author` FOREIGN KEY (`author`) REFERENCES `authors` (`id`),
   ADD CONSTRAINT `images` FOREIGN KEY (`image`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
