7e396af0121c009b3a4b7e3c146f18a46ab909a1
= <=>
2021-06-01 10:35:01 +0200

    Shop overzicht page is klaar en responsive

--- public/views/books.php
+++ public/views/books.php
@@ -1,12 +1,137 @@
-<?php
-require_once('header-footer/header.php');
-?>
-books
-<!-- voorbeeld -->
-<div id="test">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Blen Michil">
+    <link rel="stylesheet" href="public/css/books.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Shop</title>
+</head>
+
+
+<body>
+<?php require_once('header-footer/header.php'); ?>
+    
+
+
+<div class="wrapper">
+
+        <div class="main">
+            <h1 class="main__title">All Books</h1>
+            <section class="books">
+                <ul class="books__list">
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                    <!-- Tweede line boeken -->
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                    <li class="books__item">
+                        <div class="books__content">
+                            <img class="books__img" src="/public/assets/cover.jpg" alt="Book-cover">
+                            <div class="books__title"><h2>Overeen weg</h2></div>
+                            <div class="books__author">Willem Blue</div>
+                            <div class="books__price">$12.20</div>
+                            <button type="submit" class="books__btn">
+                                ADD TO CARD
+                            </button>
+                        </div>
+                    </li>
+
+                </ul>
+            </section>
+        <div>
+
 </div>
-<!-- einde voorbeeld -->
-<script src="public\scripts\getbook\get.js"></script>
-<?php
-require_once('header-footer/footer.php');
-?>
+
+
+
+<?php require_once('header-footer/footer.php'); ?>
+</body>
+
+
+</html>
