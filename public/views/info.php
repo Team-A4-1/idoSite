f51582e68c1a69be77cfe87ccf39c99f407931dd
Ltricha <34155601+Ltricha@users.noreply.github.com>
2021-06-01 09:15:32 +0200

    info update

--- public/views/info.php
+++ public/views/info.php
@@ -23,12 +23,55 @@ require_once('header-footer/header.php');
             <div class="side1">
 <figure>
     <img src="" alt=""/>
-    <div>Image</div>
+    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 469.333 469.333" style="enable-background:new 0 0 469.333 469.333;" xml:space="preserve">
+<g>
+	<g>
+		<g>
+			<path d="M234.667,170.667c-35.307,0-64,28.693-64,64s28.693,64,64,64s64-28.693,64-64S269.973,170.667,234.667,170.667z"/>
+			<path d="M234.667,74.667C128,74.667,36.907,141.013,0,234.667c36.907,93.653,128,160,234.667,160
+				c106.773,0,197.76-66.347,234.667-160C432.427,141.013,341.44,74.667,234.667,74.667z M234.667,341.333
+				c-58.88,0-106.667-47.787-106.667-106.667S175.787,128,234.667,128s106.667,47.787,106.667,106.667
+				S293.547,341.333,234.667,341.333z"/>
+		</g>
+	</g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>
 </figure>
 
 
 <div class="text-block">
-    <span class="icon"></span>
+    <span class="icon">2</span>
     <h3 class="text-block__title">Title</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Fusce gravida enim augue, venenatis commodo est venenatis eget.
@@ -37,11 +80,59 @@ require_once('header-footer/header.php');
 
 <figure>
     <img src="" alt=""/>
-    <div>Image</div>
+    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 426.667 426.667" style="enable-background:new 0 0 426.667 426.667;" xml:space="preserve">
+<g>
+	<g>
+		<g>
+			<path d="M128,341.333c-23.573,0-42.453,19.093-42.453,42.667s18.88,42.667,42.453,42.667c23.573,0,42.667-19.093,42.667-42.667
+				S151.573,341.333,128,341.333z"/>
+			<path d="M151.467,234.667H310.4c16,0,29.973-8.853,37.333-21.973L424,74.24c1.707-2.987,2.667-6.507,2.667-10.24
+				c0-11.84-9.6-21.333-21.333-21.333H89.92L69.653,0H0v42.667h42.667L119.36,204.48l-28.8,52.267
+				c-3.307,6.187-5.227,13.12-5.227,20.587C85.333,300.907,104.427,320,128,320h256v-42.667H137.067
+				c-2.987,0-5.333-2.347-5.333-5.333c0-0.96,0.213-1.813,0.64-2.56L151.467,234.667z"/>
+			<path d="M341.333,341.333c-23.573,0-42.453,19.093-42.453,42.667s18.88,42.667,42.453,42.667
+				C364.907,426.667,384,407.573,384,384S364.907,341.333,341.333,341.333z"/>
+		</g>
+	</g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>
+
 </figure>
 
-<span class="icon"></span>
+
 <div class="text-block">
+    <span class="icon">4</span>
     <h3 class="text-block__title">Title</h3>
      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Fusce gravida enim augue, venenatis commodo est venenatis eget.
@@ -56,6 +147,7 @@ require_once('header-footer/header.php');
 <div class="side2">
 
 <div class="text-block">
+<span class="icon">1</span>
     <h3 class="text-block__title">Title</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Fusce gravida enim augue, venenatis commodo est venenatis eget.
@@ -64,11 +156,13 @@ require_once('header-footer/header.php');
 
 <figure>
     <img src="" alt=""/>
-    <div>Image</div>
+    <svg id="Layer_1" enable-background="new 0 0 510 510" viewBox="0 0 510 510"  xmlns="http://www.w3.org/2000/svg"><g><path id="XMLID_225_" d="m390 270c-16.568 0-30 13.432-30 30 0-16.568-13.432-30-30-30-16.568 0-30 13.432-30 30 0-16.568-13.432-30-30-30-16.568 0-30 13.432-30 30v-135c0-16.568-13.432-30-30-30-16.568 0-30 13.432-30 30v105 75h-1.785l-28.215-37.619c-7.632-12.557-25.218-15.568-38.277-6.862-12.41 8.273-16.717 24.492-10.047 37.832l65.887 146.774c7.622 15.245 23.204 24.875 40.248 24.875h167.189c24.853 0 45-20.147 45-45v-150-15c0-16.568-13.432-30-30-30z"/><path id="XMLID_955_" d="m120 120c0-49.626 40.374-90 90-90s90 40.374 90 90c0 25.983-11.423 50.213-30 66.969v36.917c13.803-8.003 26.047-18.736 35.723-31.513 15.881-20.975 24.277-46.001 24.277-72.373 0-66.168-53.832-120-120-120s-120 53.832-120 120c0 26.372 8.395 51.398 24.277 72.373 9.676 12.776 21.92 23.51 35.723 31.513v-36.917c-18.577-16.756-30-40.986-30-66.969z"/><path id="XMLID_958_" d="m252.331 77.479c-11.329-11.279-26.35-17.479-42.328-17.479-.091 0-.184 0-.274 0-31.857.143-58.06 25.152-59.651 56.936-.441 8.812 1.108 17.504 4.341 25.502 5.189-12.734 14.624-23.299 26.547-29.921 3.378-12.79 15.271-22.455 28.896-22.517h.139c7.989 0 15.502 3.101 21.166 8.739 3.887 3.87 6.58 8.616 7.9 13.796 11.938 6.641 21.379 17.234 26.555 30.002 2.857-7.058 4.378-14.685 4.378-22.537 0-16.077-6.275-31.178-17.669-42.521z"/></g></svg>
+
 </figure>
 
 
 <div class="text-block">
+    <span class="icon">3</span>
     <h3 class="text-block__title">Title</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Fusce gravida enim augue, venenatis commodo est venenatis eget.
@@ -77,7 +171,50 @@ require_once('header-footer/header.php');
 
 <figure>
     <img src="" alt=""/>
-    <div>Image</div>
+    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 512.005 512.005" style="enable-background:new 0 0 512.005 512.005;" xml:space="preserve">
+<g>
+	<g>
+		<g>
+			<rect x="16.539" y="425.626" width="479.767" height="50.502"/>
+			<path d="M510.7,189.151c-5.429-20.201-26.135-32.195-46.335-26.766l-134.209,35.982L155.924,35.878l-48.734,13.13
+				l104.539,181.175L86.232,263.767l-49.618-39.013L0,234.603l45.957,79.667l19.317,33.457l40.528-10.858l134.209-35.983
+				l109.715-29.417l134.209-35.983C504.134,230.057,516.129,209.352,510.7,189.151z"/>
+		</g>
+	</g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>
+
 </figure>
 
 
@@ -94,6 +231,8 @@ require_once('header-footer/header.php');
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                        Fusce gravida enim augue, venenatis commodo est venenatis eget.
                        Pellentesque sit amet orci maximus, vehicula nisl nec, condimentum ex.</p>
+
+            <div class="button">Need help?  &#62; </div>
         <div>
     </section>
 </body>
